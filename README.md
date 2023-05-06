de467752f92a4bcfea736c3c937fc11843ba93e3
AVmyasoedov88 <antonmyasoedov88@gmail.com>
2023-05-06 22:26:37 +0300

    06.05.2023

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/AVmyasoedov88/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/AVmyasoedov88/frontend-project-46/actions)
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/349dcbffc69ca823cdca/maintainability)](https://codeclimate.com/github/AVmyasoedov88/frontend-project-lvl1/maintainability)
+
+[![Test Coverage](https://api.codeclimate.com/v1/badges/349dcbffc69ca823cdca/test_coverage)](https://codeclimate.com/github/AVmyasoedov88/frontend-project-lvl1/test_coverage)
---
+++ src/index.js
@@ -0,0 +1,57 @@
+import { cwd } from 'node:process';
+import fs from 'fs';
+import { log } from 'node:console';
+import _ from "lodash";
+import path from 'path';
+
+function getInf(obj1, obj2) {
+    const keys = _.uniq([...(Object.keys(obj1)), ...(Object.keys(obj2))])
+    const sortedKeys = _.sortBy(keys)
+    const result = sortedKeys.map((key) => {
+        let arr = []
+        if ((obj1[key] && obj2[key]) && (obj1[key] !== obj2[key])) {
+            const a = `- ${key}: ${obj1[key]}\n+ ${key}: ${obj2[key]}`
+            arr.push(a)
+            
+        }
+
+        if (!_.has(obj2, key)) {
+            arr.push(`- ${key}: ${obj1[key]}`)
+
+        }
+
+        if (!_.has(obj1, key)) {
+            arr.push(`+ ${key}: ${obj2[key]}`)
+
+        }
+
+        if (obj1[key] === obj2[key]) {
+            arr.push(`  ${key}: ${obj1[key]}`)
+        }
+
+        return arr
+    })
+
+    return `{\n${result.join('\n')}\n}`
+}
+
+const getPath = (filePath) => path.resolve(process.cwd(), '__fixtures__', filePath);
+
+//console.log(getPath('file1.json'))
+
+const readFile = (filePath) => fs.readFileSync(getPath(filePath), 'utf-8');
+
+//console.log(readFile('file1.json'))
+
+
+function genDiff (filePath1, filePath2) {
+  const object1 = JSON.parse(readFile(filePath1))
+  const object2 = JSON.parse(readFile(filePath2))
+  return getInf(object1, object2)
+};
+
+//gendiff('file1.json', 'file2.json')
+
+export default genDiff;
+  
+//console.log(temp) https://github.com/TopchiyVictor/frontend-project-lvl2/blob/main/src/index.js
