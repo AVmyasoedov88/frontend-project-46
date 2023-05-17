fab8c831111b1bbbb5c05c972b488b3cd5c2459d
AVmyasoedov88 <antonmyasoedov88@gmail.com>
2023-05-17 23:44:29 +0300

    add plain formatt

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -12,9 +12,17 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const path1 = getFixturePath('file1.json')
 const path2 = getFixturePath('file2.json')
+const path1Yml = getFixturePath('file1.yml')
+const path2Yml = getFixturePath('file2.yml')
 
 test('toEqual with objects', () => {
     const result = genDiff(path1, path2)
     const x = JSON.stringify(result)
     expect(x.split()).toEqual(equal)
+})
+
+test('toEqual with objects yml', () => {
+  const result = genDiff(path1Yml, path2Yml)
+  const x = JSON.stringify(result)
+  expect(x.split()).toEqual(equal)
 })
